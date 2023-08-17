705b4fd44d9ac9f714efb1eb1b9586b106f8e504
cordel22 <cordelfenevall@gmail.com>
2023-08-17 16:52:18 +0200

    first commit

---
+++ htdocs/controller/cart_session.php
@@ -0,0 +1,8 @@
+<?php
+
+if (isset($_COOKIE['SESSION'])){
+    $uid = $_COOKIE['SESSION'];
+  }else{
+    $uid = md5(uniqid('biped', true));
+  }
+  setcookie('SESSION', $uid, time() + (60 * 60 * 24 * 30));
